d49d1a77e48c01f7c280fcceee81d6692b317dd1
David Tomczyk <davidjtomczyk@gmail.com>
2016-09-25 14:01:49 -0400

    inserted ata into more tables, need to complete the last few

--- enter_xml_data.py
+++ enter_xml_data.py
@@ -65,6 +65,27 @@ def insert_2column_table(table_name, column_name, attribute):
     return cur.fetchone()[0]
 
 
+def insert_3column_table(table_name, column_name1, attribute1, column_name2, attribute2):
+    """Docstring TBD"""
+
+    cur.execute("""
+    INSERT OR IGNORE INTO {} ({}, {})
+    VALUES (?, ?)""".format(table_name, column_name1, column_name2), (attribute1, attribute2))
+    cur.execute('SELECT id FROM {} WHERE {} = ?'.format(table_name, column_name1), (attribute1, ))
+    return cur.fetchone()[0]
+
+
+def insert_4column_table(table_name, column_name1, attribute1,
+    column_name2, attribute2, column_name3, attribute3):
+    """Docstring TBD"""
+
+    cur.execute("""
+    INSERT OR IGNORE INTO {} ({}, {}, {})
+    VALUES (?, ?, ?)""".format(table_name, column_name1, column_name2, column_name3), (attribute1, attribute2, attribute3))
+    cur.execute('SELECT id FROM {} WHERE {} = ?'.format(table_name, column_name1), (attribute1, ))
+    return cur.fetchone()[0]
+
+
 #Define a function that inputs data into a link table. Utilizes a value's id (primary key)
 #returned from a previous insert function
 def insert_link_table(table_name, column1, column2, attribute1, attribute2):
@@ -89,7 +110,7 @@ for xml in get_nct_list(folderpath):
 
     start_date = active_trial.get_start_date()
 
-    intervention = active_trial.get_intervention()
+    intervention_details = active_trial.get_intervention_details()
 
     file_name = active_trial.get_file_name()
 
@@ -121,30 +142,93 @@ for xml in get_nct_list(folderpath):
 
     country = active_trial.get_country()
 
-    #Input trial's lead_sponsor into Sponsor_Type table and store corresponding id
+    #Insert trial's lead_sponsor_type into Sponsor_Type table and store corresponding id
     sponsor_type_id = insert_2column_table('Sponsor_Type', 'sponsor_type', lead_sponsor_type)
 
-    #Input trial's study_type into Study_Type table and store corresponding id
+    #Insert trial's lead_sponsor and sponsor_type_id into Sponsor table
+    #(sponsor_name and sponsor_type_id repsectively)
+    sponsor_id = insert_3column_table('Sponsor',
+                                        'sponsor_name', lead_sponsor,
+                                        'sponsor_type_id', sponsor_type_id)
+
+    #Insert trial's study_type into Study_Type table and store corresponding id
     study_type_id = insert_2column_table('Study_Type', 'study_type', study_type)
 
-    #Input trial's study_design into Study_Design table and store corresponding id
+    #Insert trial's study_design into Study_Design table and store corresponding id
     study_design_id = insert_2column_table('Study_Design', 'study_design', study_design)
 
-    #Input trial's phase into Phases table and store corresponding id
+    #Insert trial's phase into Phases table and store corresponding id
     phase_id = insert_2column_table('Phases', 'phase', phase)
 
-    #Iterate through tuple of countries in trial and input each into Country table
+    #Iterate through tuple of countries in trial and insert each into Country table
     #also store each corresponding id and enter it into Country_Link table, along with nct_id
     for item in country:
         country_id = insert_2column_table('Country', 'country', item)
         insert_link_table('Country_Link', 'country_id', 'nct_id', country_id, nct)
 
 
-    #Iterate through tuple of conditions in trial and input each into Conditions table
+    #Iterate through tuple of conditions in trial and insert each into Conditions table
     #also store each corresponding id and enter it into Conditions_Link table, along with nct_id
     for item in condition:
         condition_id = insert_2column_table('Conditions', 'condition', cb.bucket_condition(item))
         insert_link_table('Conditions_Link', 'condition_id', 'nct_id', condition_id, nct)
+
+
+    ###Debug code, delete later
+    # print 'nct number::', nct
+    # print 'intervention:\n', intervention_details
+
+    #Iterate through dicts contained within intervention_details (1 dict per intervention)
+    for item in intervention_details:
+
+        #Insert intervention's type into Intervention_Type table and store intervention_type_id
+        intervention_type_id = insert_2column_table('Intervention_Type',
+        'intervention_type', item['type'])
+
+        #Insert intervention's name, intervention_type_id and MoA_id
+        #(placeholder for now) into Interventions table
+        intervention_id = insert_4column_table('Interventions',
+        'intervention', item['intervention'],
+        'intervention_type_id', intervention_type_id,
+        'moa_id', 999)
+
+        #Iterate through items contained within other_name tuple and insert
+        #them into Intervention_Other_Names table, along with the corresponding
+        #intervention_id
+        for name in item['other_name']:
+            insert_3column_table('Intervention_Other_Names',
+            'other_name', name,
+            'intervention_id', intervention_id)
+
+    # Input data into Trials table
+    cur.execute("""INSERT OR IGNORE INTO Trials (nct_id,
+                                                phase_id,
+                                                status,
+                                                study_type_id,
+                                                study_design_id,
+                                                start_date,
+                                                completion_date,
+                                                primary_completion_date,
+                                                verification_date,
+                                                last_changed_date,
+                                                sponsor_id)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", (nct,
+                                                    phase_id,
+                                                    status,
+                                                    study_type_id,
+                                                    study_design_id,
+                                                    start_date,
+                                                    completion_date,
+                                                    primary_completion_date,
+                                                    verification_date,
+                                                    last_changed_date,
+                                                    sponsor_id))
+
+    ###Debug code, delete later
+    #print '---'
+
+
+    #Tuple of dicts, each dict represents the info for one intervention in the trial (strings AND tuples are values)
 
 
     #Intervention dict info to enter: intervention_name, type, study_arm, and other_name
