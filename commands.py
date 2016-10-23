f1ecbef991e6e42a63dc7b2a9e177d00c29f7f96
David Tomczyk <davidjtomczyk@gmail.com>
2016-10-23 16:40:42 -0400

    created python script to quickly run my other files/modules without calling them directly

---
+++ commands.py
@@ -0,0 +1,45 @@
+"""Program to allow easier executing of program actions/modules"""
+
+from create_trial import Trial
+import create_db
+import enter_xml_data
+import test_script
+import csv_data
+
+def run_controlpanel():
+
+    instructions = '''Commands:
+    'createdb' = python create_db.py
+    'enterdata' = python enter_xml_data.py
+    'test' = python test_script.py
+    'c' = print commands
+    'q' = exit
+    '''
+
+    print instructions
+
+    while True:
+
+        command = raw_input('Enter command:')
+
+        if command == 'createdb':
+            create_db.create_database()
+
+        elif command == 'enterdata':
+            enter_xml_data.enter_xml_data()
+
+        elif command == 'test':
+            test_script.main()
+
+        elif command == 'c':
+            print instructions
+
+        elif command == 'q':
+            break
+
+        else:
+            print 'Command not recognized\n'
+
+
+if __name__ == '__main__':
+    run_controlpanel()
