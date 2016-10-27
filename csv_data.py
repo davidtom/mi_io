a761e4441c028c5cb670758948b7b4706b86cf34
David Tomczyk <davidjtomczyk@gmail.com>
2016-10-26 22:55:28 -0400

    moa now retrieved from csv; added function to query tables for a value in one column and return another; need to now add interventions with moa_id == NULL to csv, and make sure there are no duplicates

--- csv_data.py
+++ csv_data.py
@@ -1,6 +1,14 @@
 """TBD"""
 
 import csv
+import sqlite3
+
+#Define files accessed in module
+dbname = 'main_db.sqlite3'
+
+#Connect to db
+conn = sqlite3.connect(dbname)
+cur = conn.cursor()
 
 ###Create functions to recategorize a specific or differently named condition
 ###into a broader condition 'bucket' (ie NSCLC rather than non-small cell lung cancer)
@@ -65,6 +73,50 @@ def sync_moa_list():
 
 
 
+##def
+#########TO DO
+#1) Clean up moa_list.csv. Use python to help clean the data! (ie splitting up rows, making sure no repeats)
+#1.5) Recreate database and enter full data set into it
+#2) Create function to query a 4 column table (or any dimension table, right?) based on the value of an
+#### attribute (query_table(TABLENAME, COLUMNNAME, ATTRIBUTETOSEARCHFOR
+#### or something))
+#3) Use output of above, which returns the name of the attributes == ATTRIBUTETOSEARCHFOR
+#### to add rows to moa_list.csv. Again make sure there are no duplicates
+#4) Create a function that will update a table (id change Null to a value; dont use INSERT IGNORE)
+#### perhaps make this dimension flexible too if possible, will be able to use this to update
+#### all tables (either outright or as a template)
+
+#Create a function that searches a column in a table to find values equal to
+# a specified value. Returns the value of a specified row in that column ('id' by default)
+#Returns the value(s) as a list of tuples
+def get_value(table_name, column_name, search_value, column ='id'):
+    """TBD"""
+
+    #Run query
+    cur.execute("""SELECT {} FROM {} WHERE {} = ?""".format(column, table_name, column_name), (search_value,))
+
+    #Check to make sure only one id is returned, otherwise data assumptions are wrong
+    query_results = cur.fetchall()
+
+    if len(query_results) == 1:
+        #return the first value (and only value), of the first tuple (and only tuple)
+        return query_results[0][0]
+    if len(query_results) > 1:
+        return query_results
+    else:
+        raise ValueError('query_results has len(value) == 0; it is an empty list')
+
+    return None
+
+###Per above to do list, now I need to enter the interventions with no MoA id into csv file
+def add_to_csv(): ##VARIABLES TBD
+    """TBD"""
+
+    ##Need to manipulate this list:
+    values = get_value('Interventions', 'moa_id', get_value('MoA', 'moa', 'NULL'), 'intervention')
+    print values
+    print len(values)
+
 
 def get_moa(intervention, data = 'moa_list.csv'):
     """TBD"""
@@ -79,7 +131,7 @@ def get_moa(intervention, data = 'moa_list.csv'):
             else:
                 pass
 
-    return None
+    return 'NULL'
 
 
 ###
