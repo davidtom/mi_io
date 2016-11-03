7cce7cbd825e5f2eb786958e2b6cbc89e9b9072c
David Tomczyk <davidjtomczyk@gmail.com>
2016-11-03 19:55:17 -0400

    initial commit

---
+++ db_class.py
@@ -0,0 +1,18 @@
+"""TBD Class that creates a database object"""
+
+class DB(object):
+    """TBD"""
+
+#import modules
+import sqlite3
+import os
+import re
+import csv
+import create_trial as ct
+import csv_data
+
+#
+class DB(object):
+    """TBD"""
+
+    
