93e2855177d685c0895e5f61ac71d71d3382c26a
David Tomczyk <davidjtomczyk@gmail.com>
2016-11-11 23:43:06 -0500

    commented out redundant code that has been moved to db_class.py. Maintaining for reference

--- moa_db_mgmt.py
+++ moa_db_mgmt.py
@@ -28,62 +28,62 @@ conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 
 
-#Create function that reads data from a csv (moa_list.csv by default)
-#and enters all its data into the database
-#Note:
-#   *assumes structure of: agent,moa, moa_bucket,source
-#   *INSERT OR IGNORE - cannot overwrite data with this function
-def enter_csv_data(csv_file = 'moa_list.csv', headers = True):
+# #Create function that reads data from a csv (moa_list.csv by default)
+# #and enters all its data into the database
+# #Note:
+# #   *assumes structure of: agent,moa, moa_bucket,source
+# #   *INSERT OR IGNORE - cannot overwrite data with this function
+# def enter_csv_data(csv_file = 'moa_list.csv', headers = True):
+#
+#     #Open csv_file
+#     with open(csv_file, 'rU+') as csv_f:
+#
+#         #Read contents of csv_file
+#         f = csv.reader(csv_f, delimiter=',')
+#
+#         #Strip/store headers from csv_file if they exist
+#         headers_str = None
+#         if headers:
+#             headers_str = f.next()
+#
+#         #Iterate through rows of csv_file and insert data into
+#         #appropriate Tables
+#
+#         for row in f:
+#
+#             agent = row[0].lower().strip()
+#             moa = row[1].lower().strip()
+#             macro_moa = row[2].lower().strip()
+#             source = row[3].lower().strip()
+#
+#             ##Debugging Code
+#             # print 'agent: {}'.format(agent)
+#             # print 'moa: {}'.format(moa)
+#             # print 'macro_moa: {}'.format(macro_moa)
+#             # print 'source: {}'.format(source)
+#             # print '---'
+#
+#             #Insert data into Macro_MoA table and get associated id
+#             macro_moa_id = exd.insert_2column_table('Macro_MoA',
+#                                                     'macro_moa', macro_moa, cur)
+#
+#             #Insert data into MoA table and get associated id
+#             moa_id = exd.insert_3column_table('MoA',
+#                                             'moa', moa,
+#                                             'macro_moa_id', macro_moa_id, cur)
+#
+#             #Insert data into Agent table
+#             cur.execute("""INSERT OR IGNORE INTO Agent (agent,
+#                                                         moa_id,
+#                                                         source)
+#                             VALUES (?, ?, ?)""", (agent,
+#                                                     moa_id,
+#                                                     source))
+#
+#             conn.commit()
+#
+#     return None
 
-    #Open csv_file
-    with open(csv_file, 'rU+') as csv_f:
 
-        #Read contents of csv_file
-        f = csv.reader(csv_f, delimiter=',')
-
-        #Strip/store headers from csv_file if they exist
-        headers_str = None
-        if headers:
-            headers_str = f.next()
-
-        #Iterate through rows of csv_file and insert data into
-        #appropriate Tables
-
-        for row in f:
-
-            agent = row[0].lower().strip()
-            moa = row[1].lower().strip()
-            macro_moa = row[2].lower().strip()
-            source = row[3].lower().strip()
-
-            ##Debugging Code
-            # print 'agent: {}'.format(agent)
-            # print 'moa: {}'.format(moa)
-            # print 'macro_moa: {}'.format(macro_moa)
-            # print 'source: {}'.format(source)
-            # print '---'
-
-            #Insert data into Macro_MoA table and get associated id
-            macro_moa_id = exd.insert_2column_table('Macro_MoA',
-                                                    'macro_moa', macro_moa, cur)
-
-            #Insert data into MoA table and get associated id
-            moa_id = exd.insert_3column_table('MoA',
-                                            'moa', moa,
-                                            'macro_moa_id', macro_moa_id, cur)
-
-            #Insert data into Agent table
-            cur.execute("""INSERT OR IGNORE INTO Agent (agent,
-                                                        moa_id,
-                                                        source)
-                            VALUES (?, ?, ?)""", (agent,
-                                                    moa_id,
-                                                    source))
-
-            conn.commit()
-
-    return None
-
-
-if __name__ == '__main__':
-    enter_csv_data()
+# if __name__ == '__main__':
+#     enter_csv_data()
