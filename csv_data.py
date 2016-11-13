d1642a076d793d07bbbbec41beae2e2111577557
David Tomczyk <davidjtomczyk@gmail.com>
2016-11-13 09:01:45 -0500

    commented out functions that have been moved to db_class.py

--- csv_data.py
+++ csv_data.py
@@ -17,56 +17,56 @@ cur = conn.cursor()
 
 ###Create functions to recategorize a specific or differently named condition
 ###into a broader condition 'bucket' (ie NSCLC rather than non-small cell lung cancer)
-def bucket_conditions(condition):
-
-    #Define lists to hold name of conditions in a bucket
-    NSCLC_list = list()
-    RCC_list = list()
-    melanoma_list = list()
-    prostate_cancer_list = list()
-
-    #open conditions.csv and populate condition buckets
-    with open('condition_buckets.csv', 'rU') as csv_file:
-        f = csv.reader(csv_file, delimiter=',')
-
-        for row in f:
-
-            #Fill out NSCLC_list
-            if row[1] == 'NSCLC':
-                NSCLC_list.append(row[0])
-
-            #Fill out RCC_list
-            if row[1] == 'RCC':
-                RCC_list.append(row[0])
-
-            #Fill out RCC_list
-            if row[1] == 'melanoma':
-                melanoma_list.append(row[0])
-
-            #Fill out prostate_cancer_list
-            if row[1] == 'prostate Cancer':
-                prostate_cancer_list.append(row[0])
-
-
-    #Check if condition matches to NSCLC_list in cb module
-    if condition in NSCLC_list:
-        return 'NSCLC'
-
-    #Check if condition matches to RCC_list in cb module
-    elif condition in RCC_list:
-        return 'Renal Cell Carcinoma'
-
-    #Check if condition matches to melanoma_list in cb module
-    elif condition in melanoma_list:
-        return 'Melanoma'
-
-    #Check if condition matches to melanoma_list in cb module
-    elif condition in prostate_cancer_list:
-        return 'Prostate Cancer'
-
-    #Return original condition since condition matched to no buckets in cb module
-    else:
-        return condition
+# def bucket_conditions(condition):
+#
+#     #Define lists to hold name of conditions in a bucket
+#     NSCLC_list = list()
+#     RCC_list = list()
+#     melanoma_list = list()
+#     prostate_cancer_list = list()
+#
+#     #open conditions.csv and populate condition buckets
+#     with open('condition_buckets.csv', 'rU') as csv_file:
+#         f = csv.reader(csv_file, delimiter=',')
+#
+#         for row in f:
+#
+#             #Fill out NSCLC_list
+#             if row[1] == 'NSCLC':
+#                 NSCLC_list.append(row[0])
+#
+#             #Fill out RCC_list
+#             if row[1] == 'RCC':
+#                 RCC_list.append(row[0])
+#
+#             #Fill out RCC_list
+#             if row[1] == 'melanoma':
+#                 melanoma_list.append(row[0])
+#
+#             #Fill out prostate_cancer_list
+#             if row[1] == 'prostate Cancer':
+#                 prostate_cancer_list.append(row[0])
+#
+#
+#     #Check if condition matches to NSCLC_list in cb module
+#     if condition in NSCLC_list:
+#         return 'NSCLC'
+#
+#     #Check if condition matches to RCC_list in cb module
+#     elif condition in RCC_list:
+#         return 'Renal Cell Carcinoma'
+#
+#     #Check if condition matches to melanoma_list in cb module
+#     elif condition in melanoma_list:
+#         return 'Melanoma'
+#
+#     #Check if condition matches to melanoma_list in cb module
+#     elif condition in prostate_cancer_list:
+#         return 'Prostate Cancer'
+#
+#     #Return original condition since condition matched to no buckets in cb module
+#     else:
+#         return condition
 
 
 ##Create function to read data from moa_list.csv and use it to assign
@@ -141,20 +141,20 @@ def create_csv(query_results, csv_file = 'interventions_data.csv', sort_first_co
 ##database. Then get_moa should query that database for an intervention, and return its MoA
 ###The intervention database will have more interventions than the Interventions table in my database
 ###Interventions with NULL MoAs in function above should then be added to the new database
-def get_moa(intervention, data = 'moa_list.csv'):
-    """TBD"""
-
-    with open(data, 'rU+') as csv_file:
-
-        f = csv.reader(csv_file, delimiter=',')
-
-        for row in f:
-            if intervention.lower().strip() == row[0].lower().strip():
-                return row[1].lower().strip()
-            else:
-                pass
-
-    return 'NULL'
+# def get_moa(intervention, data = 'moa_list.csv'):
+#     """TBD"""
+#
+#     with open(data, 'rU+') as csv_file:
+#
+#         f = csv.reader(csv_file, delimiter=',')
+#
+#         for row in f:
+#             if intervention.lower().strip() == row[0].lower().strip():
+#                 return row[1].lower().strip()
+#             else:
+#                 pass
+#
+#     return 'NULL'
 
 
 ###
